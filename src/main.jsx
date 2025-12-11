b8bf7d1684207ba12a56ed5deace305fd3ac165f
Yashdeft <yashwanth.bandaru@deftsquad.com>
2025-12-11 16:04:49 -0500

    Initial Code

---
+++ src/main.jsx
@@ -0,0 +1,15 @@
+
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import { BrowserRouter } from 'react-router-dom'
+import ManageData from './api/WillContext.jsx'
+import BeneficiaryRoutes from './BeneficiaryRoutes.jsx'
+createRoot(document.getElementById('root')).render(
+
+     <BrowserRouter>
+   
+       <BeneficiaryRoutes />
+   
+    </BrowserRouter>
+ 
+)
